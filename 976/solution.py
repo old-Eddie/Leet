00de3b5dce011d802331b965db5ada068be0ac4a
Old-eddie <amiraliahmadi220@gmail.com>
2025-09-28 12:59:55 +0330

    976📗

---
+++ 976/solution.py
@@ -0,0 +1,24 @@
+from typing import List
+
+
+class Solution:
+
+    def largestPerimeter(self, nums: List[int]) -> int:
+        nums.sort()
+        ans = []
+        max = 0
+        for i in range(len(nums)-2):
+            if (nums[i] + nums[i+1] > nums[i+2]):
+                ans.append(i)
+            else:
+                None
+        if len(ans) > 0:
+            for i in ans:
+                if nums[i]+nums[i+1]+nums[i+2] > max:
+                    max = nums[i]+nums[i+1]+nums[i+2]
+        return max
+
+
+solution = Solution()
+ans = solution.largestPerimeter(nums=[3, 2, 3, 4])
+print(ans)
