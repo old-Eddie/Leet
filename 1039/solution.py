c29d6b9fedb8ab64ae3f4ea17468a3a61cd203a2
Old-eddie <amiraliahmadi220@gmail.com>
2025-09-29 14:50:51 +0330

    1039🟨

---
+++ 1039/solution.py
@@ -0,0 +1,21 @@
+from functools import cache
+from typing import List
+
+class Solution:
+    def minScoreTriangulation(self, values: List[int]) -> int:
+        @cache
+        def dp(i, j):
+            if j-i < 2:
+                return 0
+
+            res = float("inf")
+            for k in range(i+1, j):
+                curr = values[i]*values[j]*values[k]
+                res = min(res, curr + dp(i, k) + dp(k, j))
+            return res
+        return dp(0, len(values)-1)
+
+sol = Solution()
+values = [1, 2, 3]
+ans = sol.minScoreTriangulation(values)
+print(ans)
